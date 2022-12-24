0ce4a38f2ad913bf4d078f1890cdaae2f1b75526
FrCln <atchistov@gmail.com>
2022-12-24 17:26:57 +0300

    create simple test client

---
+++ bookkeeper/simple_client.py
@@ -0,0 +1,42 @@
+"""
+Простой тестовый скрипт для терминала
+"""
+
+from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
+from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.utils import read_tree
+
+cat_repo = MemoryRepository[Category]()
+exp_repo = MemoryRepository[Expense]()
+
+cats = '''
+продукты
+    мясо
+        сырое мясо
+        мясные продукты
+    сладости
+книги
+одежда
+'''.splitlines()
+
+Category.create_from_tree(read_tree(cats), cat_repo)
+
+while True:
+    cmd = input('$> ')
+    if not cmd:
+        break
+    if cmd == 'категории':
+        print(*cat_repo.get_all(), sep='\n')
+    elif cmd == 'расходы':
+        print(*exp_repo.get_all(), sep='\n')
+    elif cmd[0].isdecimal():
+        amount, name = cmd.split(maxsplit=1)
+        try:
+            cat = cat_repo.get_all({'name': name})[0]
+        except IndexError:
+            print(f'категория {name} не найдена')
+            continue
+        exp = Expense(int(amount), cat.pk)  # type: ignore [arg-type]
+        exp_repo.add(exp)
+        print(exp)
