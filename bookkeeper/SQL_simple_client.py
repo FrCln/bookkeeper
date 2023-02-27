36189605f3d7eb380f59b667ac109f591bda241c
Kim Maxim <kim.imax0721@yandex.ru>
2023-02-27 13:48:11 +0200

    added SQLite version of simple_client

---
+++ bookkeeper/SQL_simple_client.py
@@ -0,0 +1,45 @@
+"""
+Простой тестовый скрипт для терминала
+"""
+
+from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
+from bookkeeper.repository.sqlite_repository import SQLiteRepository
+from bookkeeper.utils import read_tree
+
+cat_repo = SQLiteRepository('budget.db', Category)
+exp_repo = SQLiteRepository('budget.db', Expense)
+
+cats = '''
+продукты
+    мясо
+        сырое мясо
+        мясные продукты
+    сладости
+книги
+одежда
+'''.splitlines()
+
+Category.create_from_tree(read_tree(cats), cat_repo)
+
+while True:
+    try:
+        cmd = input('$> ')
+    except EOFError:
+        break
+    if not cmd:
+        continue
+    if cmd == 'категории':
+        print(*cat_repo.get_all(), sep='\n')
+    elif cmd == 'расходы':
+        print(*exp_repo.get_all(), sep='\n')
+    elif cmd[0].isdecimal():
+        amount, name = cmd.split(maxsplit=1)
+        try:
+            cat = cat_repo.get_all({'name': name})[0]
+        except IndexError:
+            print(f'категория {name} не найдена')
+            continue
+        exp = Expense(int(amount), cat.pk)
+        exp_repo.add(exp)
+        print(exp)
