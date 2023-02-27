1b3b1634655a6ca349c2152366d875a67aa8a441
Kim Maxim <kim.imax0721@yandex.ru>
2023-02-27 15:22:28 +0200

    created two separate clients for the SQL repository and the RAM repository

---
+++ bookkeeper/cats_for_simple_clients.py
@@ -0,0 +1,40 @@
+from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
+from bookkeeper.utils import read_tree
+from bookkeeper.repository.abstract_repository import AbstractRepository
+
+
+def run_bookkeeper(cat_repo: AbstractRepository[Category], exp_repo: AbstractRepository[Expense]) -> None:
+    cats = '''
+    продукты
+        мясо
+            сырое мясо
+            мясные продукты
+        сладости
+    книги
+    одежда
+    '''.splitlines()
+
+    Category.create_from_tree(read_tree(cats), cat_repo)
+
+    while True:
+        try:
+            cmd = input('$> ')
+        except EOFError:
+            break
+        if not cmd:
+            continue
+        if cmd == 'категории':
+            print(*cat_repo.get_all(), sep='\n')
+        elif cmd == 'расходы':
+            print(*exp_repo.get_all(), sep='\n')
+        elif cmd[0].isdecimal():
+            amount, name = cmd.split(maxsplit=1)
+            try:
+                cat = cat_repo.get_all({'name': name})[0]
+            except IndexError:
+                print(f'категория {name} не найдена')
+                continue
+            exp = Expense(int(amount), cat.pk)
+            exp_repo.add(exp)
+            print(exp)
--- bookkeeper/sql_simple_client.py
+++ bookkeeper/sql_simple_client.py
@@ -4,10 +4,11 @@
 
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
-from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.repository.sqlite_repository import SQLiteRepository
 from bookkeeper.cats_for_simple_clients import run_bookkeeper
 
-cat_repo = MemoryRepository[Category]()
-exp_repo = MemoryRepository[Expense]()
+
+cat_repo = SQLiteRepository('budget.db', Category)
+exp_repo = SQLiteRepository('budget.db', Expense)
 
 run_bookkeeper(cat_repo, exp_repo)
