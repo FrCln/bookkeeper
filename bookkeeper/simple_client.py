c161a0005c5febf19ce99de71e538d6dec100a22
Kim Maxim <kim.imax0721@yandex.ru>
2023-03-03 00:25:17 +0200

    updated simple clients, added a budget there

--- bookkeeper/simple_client.py
+++ bookkeeper/simple_client.py
@@ -5,10 +5,12 @@
 
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
+from bookkeeper.models.budget import Budget
 from bookkeeper.repository.memory_repository import MemoryRepository
 from bookkeeper.run_simple_client import run_simple_client
 
-cat_repo = MemoryRepository[Category]()
-exp_repo = MemoryRepository[Expense]()
+cat_repo: MemoryRepository[Category] = MemoryRepository()
+exp_repo: MemoryRepository[Expense] = MemoryRepository()
+bud_repo: MemoryRepository[Budget] = MemoryRepository()
 
-run_simple_client(cat_repo, exp_repo)
+run_simple_client(cat_repo, exp_repo, bud_repo)
