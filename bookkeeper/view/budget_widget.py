e0aa3f4c0df7155475ad0793c4d76d82ed2ac3d5
Kim Maxim <kim.imax0721@yandex.ru>
2023-03-13 14:33:27 +0200

    VIEW changed the budget widget

--- bookkeeper/view/budget_widget.py
+++ bookkeeper/view/budget_widget.py
@@ -1,98 +1,128 @@
 """
 Модуль содержит класс бюджетного виджета для отображения информации о бюджете.
 """
+from PyQt6.QtCore import QDate
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QLabel,
-    QDoubleSpinBox,
+    QLineEdit,
     QGroupBox,
     QHBoxLayout,
     QRadioButton,
     QPushButton,
+    QGridLayout,
 )
 
 
 class BudgetWidget(QWidget):
     """
-    Виджет для отображения бюджетной информации
-    за разные периоды (день, неделю и месяц)
+    Виджет для отображения информации о бюджете
+    за разные периоды (день, неделю и месяц).
     """
+
     def __init__(self) -> None:
         super().__init__()
 
-        # создаёт поле для хранения настроек бюджета
-        group_box = QGroupBox("Бюджет")
-        group_layout = QVBoxLayout()
+        current_month = QDate.currentDate().toString("MMMM yyyy")
+        self.month_label = QLabel(f"Бюджет за {current_month}")
+        self.month_label.setStyleSheet("font-size: 24px; font-weight: bold;")
 
-        # для отображения текущего бюджета
-        self.budget_label = QLabel("Текущий бюджет: "
-                                   "\u20bd0.00")  # символ российского рубля! Z
-        group_layout.addWidget(self.budget_label)
+        self.month_budget_label = QLabel("\u20bd0.00")
+        self.month_budget_label.setStyleSheet("font-size: 18px;")
 
-        # переключатели для выбора периода времени для бюджета
-        self.day_button = QRadioButton("День")
-        self.week_button = QRadioButton("Неделя")
-        self.month_button = QRadioButton("Месяц")
-        self.day_button.setChecked(True)  # день стоит по дефолту
+        group_box = QGroupBox("БЮДЖЕТ:")
+        group_layout = QGridLayout()
 
-        time_period_layout = QHBoxLayout()
-        time_period_layout.addWidget(self.day_button)
-        time_period_layout.addWidget(self.week_button)
-        time_period_layout.addWidget(self.month_button)
+        day_budget_label = QLabel("Бюджет на день:")
+        day_budget_label.setStyleSheet("font-size: 16px;")
+        self.day_budget_edit = QLineEdit()
+        self.day_budget_edit.setPlaceholderText("\u20bd0.00")
 
-        group_layout.addLayout(time_period_layout)
+        week_budget_label = QLabel("Бюджет на неделю:")
+        week_budget_label.setStyleSheet("font-size: 16px;")
+        self.week_budget_edit = QLineEdit()
+        self.week_budget_edit.setPlaceholderText("\u20bd0.00")
 
-        # создаёте QDoubleSpinBox для установки бюджета
-        self.budget_spinbox = QDoubleSpinBox()
-        self.budget_spinbox.setPrefix("\u20bd")
-        self.budget_spinbox.setDecimals(2)
-        self.budget_spinbox.setMinimum(0.0)
-        self.budget_spinbox.setMaximum(100000000000000000000.0)
-        self.budget_spinbox.setValue(0.0)
+        month_budget_label = QLabel("Бюджет на месяц:")
+        month_budget_label.setStyleSheet("font-size: 16px;")
+        self.month_budget_edit = QLineEdit()
+        self.month_budget_edit.setPlaceholderText("\u20bd0.00")
 
-        group_layout.addWidget(self.budget_spinbox)
-
-        # кнопка для подтверждения изменений
-        apply_button = QPushButton("Применить")
-        apply_button.clicked.connect(self.apply_budget)
-
-        group_layout.addWidget(apply_button)
-
+        group_layout.addWidget(day_budget_label, 0, 0)
+        group_layout.addWidget(self.day_budget_edit, 0, 1)
+        group_layout.addWidget(week_budget_label, 1, 0)
+        group_layout.addWidget(self.week_budget_edit, 1, 1)
+        group_layout.addWidget(month_budget_label, 2, 0)
+        group_layout.addWidget(self.month_budget_edit, 2, 1)
         group_box.setLayout(group_layout)
 
+        expenses_group_box = QGroupBox("РАСХОДЫ:")
+        expenses_layout = QVBoxLayout()
+        expenses_group_box.setLayout(expenses_layout)
+
+        self.day_expenses_label = QLabel("Расходы за день: \u20bd0.00")
+        self.week_expenses_label = QLabel("Расходы за неделю: \u20bd0.00")
+        self.month_expenses_label = QLabel("Расходы за месяц: \u20bd0.00")
+        expenses_layout.addWidget(self.day_expenses_label)
+        expenses_layout.addWidget(self.week_expenses_label)
+        expenses_layout.addWidget(self.month_expenses_label)
+
         layout = QVBoxLayout()
+        layout.addWidget(self.month_label)
+        layout.addWidget(self.month_budget_label)
         layout.addWidget(group_box)
+        layout.addWidget(expenses_group_box)
         self.setLayout(layout)
 
-        # соединяем сигналы с методами обновления
-        self.day_button.toggled.connect(self.update_budget_label)
-        self.week_button.toggled.connect(self.update_budget_label)
-        self.month_button.toggled.connect(self.update_budget_label)
-        self.budget_spinbox.valueChanged.connect(self.update_budget_label)
+        self.day_budget_edit.textChanged.connect(self.update_day_budget)
+        self.week_budget_edit.textChanged.connect(self.update_week_budget)
+        self.month_budget_edit.textChanged.connect(self.update_month_budget)
 
-    def apply_budget(self) -> None:
+    def set_month_budget(self, value: float) -> None:
         """
-        Метод для обработки нажатия кнопки "Применить"
+        Устанавливает бюджет на месяц заголовком
         """
-        # здесь добавлю логику для сохранения настроек бюджета
-        pass
+        self.month_budget_label.setText(f"Month Budget: \u20bd{value:.2f}")
 
-    def update_budget_label(self) -> None:
+    def set_month(self, year: int, month: int) -> None:
         """
-        Метод для обновления текста метки текущего бюджета
+        Устанавливает какой месяц и день отображать
         """
-        budget = self.budget_spinbox.value()
+        month_name = QDate(year, month, 1).toString("MMMM yyyy")
+        self.month_label.setText(f"Budget for {month_name}")
 
-        if self.day_button.isChecked():
-            period = "День"
-            budget_per_period = budget / 30
-        elif self.week_button.isChecked():
-            period = "Неделя"
-            budget_per_period = budget / 7
-        else:
-            period = "Месяц"
-            budget_per_period = budget
+    def update_day_budget(self, value: str) -> None:
+        """
+        Метод для обновления бюджета за день
+        """
+        self.update_expenses_labels()
 
-        self.budget_label.setText(
-            f"Текущий бюджет ({period}): \u20bd{budget_per_period:.2f}")
+    def update_week_budget(self, value: str) -> None:
+        """
+        Метод для обновления недельного бюджета
+        """
+        self.update_expenses_labels()
+
+    def update_month_budget(self, value: str) -> None:
+        """
+        Метод для обновления месячного бюджета
+        """
+        try:
+            value = float(value)
+        except ValueError:
+            value = 0.0
+        self.month_budget_label.setText(f"Бюджет за месяц: \u20bd{value:.2f}")
+        self.update_expenses_labels()
+
+    def update_expenses_labels(self) -> None:
+        """
+        Метод для обновления месячных расходов
+        """
+        # TODO: расходы в бюджетном виджете
+        day_expenses = 0.0
+        week_expenses = 0.0
+        month_expenses = 0.0
+        self.day_expenses_label.setText(f"Расходы за день: \u20bd{day_expenses:.2f}")
+        self.week_expenses_label.setText(f"Расходы за неделю: \u20bd{week_expenses:.2f}")
+        self.month_expenses_label.setText(f"Расходы за месяц: \u20bd{month_expenses:.2f}")
